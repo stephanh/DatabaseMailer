982630a6eedb839b36ec4176ad9704e516c9a941
Stephan <stephan@home.com>
2011-12-13 21:15:06 +1100

    Added 3 level templating and asynchronus queries.

--- build.sbt
+++ build.sbt
@@ -17,11 +17,13 @@ resolvers += "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots/"
 resolvers += "Querulous" at "http://maven.twttr.com/"
 
 libraryDependencies ++= Seq(
+  "javax.mail" % "mail" % "1.4.4",
   "com.twitter" % "querulous" % "2.6.5",
+  "com.twitter" %% "util" % "1.12.8",
   "org.clapper" %% "grizzled-scala" % "1.0.9",
+  "org.clapper" %% "grizzled-slf4j" % "0.6.6",
   "org.fusesource.scalate" % "scalate-wikitext" % "1.5.3",
   "org.fusesource.scalate" % "scalate-page" % "1.5.3",
   "org.scala-tools.testing" %% "specs" % "1.6.9" % "test->default",
-  "mysql" % "mysql-connector-java" % "5.1.15" % "compile->default",
   "ch.qos.logback" % "logback-classic" % "0.9.27" % "compile->default"
 )
--- src/main/scala/com/github/stephanh/databasemailer/DatabaseMailer.scala
+++ src/main/scala/com/github/stephanh/databasemailer/DatabaseMailer.scala
@@ -1,56 +1,62 @@
 import scala.io.Source
-import scala.collection.Map
-import scala.xml.{XML, NodeSeq}
+import scala.collection.immutable.Map
+import scala.xml.{XML, NodeSeq, Elem}
+
+import javax.mail._
+import javax.mail.internet._
+import javax.mail.Authenticator
+import javax.mail.PasswordAuthentication
+import java.util.Properties
 
 import grizzled.config.{Configuration, ConfigException, Section}
+import grizzled.slf4j.Logging
 
 import com.twitter.querulous.evaluator.QueryEvaluator
+import com.twitter.querulous.async.AsyncQueryEvaluator
+import com.twitter.util.Future
+import com.twitter.conversions.time._
 
 import org.fusesource.scalate.scuery._
 
-object DatabaseMailer {
+object DatabaseMailer extends Logging {
   val querySectionPrefix = "query_"
+  val emailSectionPrefix = "email_"
+
 
   def main(args: Array[String]) {
     val configFile = args(0)
 
     val config = Configuration(Source.fromFile(configFile))
 
-    val defaultDBSettings = DBConnectionSettings(config.getSection("general").getOrElse(throw new ConfigException("Need general selection in config file")))
+    val dbSettings = DBConnectionSettings(config.getSection("general").getOrElse(throw new ConfigException("Need general selection in config file")))
+    val queryEvaluator = AsyncQueryEvaluator(dbSettings.host, dbSettings.user, dbSettings.password)
 
+    val generalTemplate = XML.loadFile(config.get("general", "template").getOrElse(throw new ConfigException("Need template attribute in general section")))
 
     val querySectionRegex = ("^" + querySectionPrefix + ".*").r
-    config.forMatchingSections(querySectionRegex)(processQueries(defaultDBSettings))
+    val queryResults: Map[String, Future[NodeSeq]] = config.matchingSections(querySectionRegex) map processQueries(queryEvaluator) toMap
+
+    val emailSectionRegex = ("^" + emailSectionPrefix + ".*").r
+    config.matchingSections(emailSectionRegex).par foreach sendEmail(queryResults, generalTemplate)
 
   }
 
-  def processQueries(dbSettings: DBConnectionSettings)(section: Section) {
-    
 
-    new QueryRender(section.name.drop(querySectionPrefix length),
-                    section.options.get("sql").getOrElse(throw new ConfigException("%s needs to include \"sql\" attribute" format (section.name))),
-                    section.options.get("template").getOrElse(throw new ConfigException("%s needs to include \"template\" attribute" format (section.name))),
-                    dbSettings)
+  def processQueries(queryEvaluator: AsyncQueryEvaluator)(section: Section): (String, Future[NodeSeq]) = {
+    info("Processing query " + section.name)
+
+    val sql = section.options.get("sql").getOrElse(throw new ConfigException("%s needs to include \"sql\" attribute" format (section.name)))
+    val templateFilename = section.options.get("template").getOrElse(throw new ConfigException("%s needs to include \"template\" attribute" format (section.name)))
+
+
+
+    val result = runQuery(sql, queryEvaluator).map(renderQuery(templateFilename))
+
+    (section.name -> result)
   }
-}
 
-case class DBConnectionSettings(val host: String, val user: String, val password: String = "", val port: Int = 3306)
 
-object DBConnectionSettings {
-  def apply(section: Section): DBConnectionSettings =
-    new DBConnectionSettings(section.options.get("db.host").getOrElse(throw new ConfigException("%s needs to include \"db.host\" attribute" format (section.name))),
-                             section.options.get("db.username").getOrElse(throw new ConfigException("%s needs to include \"db.username\" attribute" format (section.name))),
-                             section.options.getOrElse("db.password", ""),
-                             section.options.getOrElse("db.port", "3306").toInt)
-}
-    
-
-class QueryRender(name: String, query: String, templateFile: String, dbSettings: DBConnectionSettings) {
-  val queryOutput = renderQuery(runQuery(query, dbSettings), templateFile)
-
-  def runQuery(query: String, dbSettings: DBConnectionSettings): Seq[Map[String, String]] = { 
-    val queryEvaluator = QueryEvaluator(dbSettings.host, dbSettings.user, dbSettings.password) 
-    val result: Seq[Map[String, String]] = queryEvaluator.select(query) {
+  def runQuery(query: String, queryEvaluator: AsyncQueryEvaluator): Future[Seq[Map[String, String]]] = queryEvaluator.select(query) {
       row => {
         val metaData = row.getMetaData
         (for (i <- 1 to metaData.getColumnCount;
@@ -59,16 +65,42 @@ class QueryRender(name: String, query: String, templateFile: String, dbSettings:
               key <- List(label, "col" + i.toString))
          yield (key -> value)) toMap
       }
-    }
-    
-    result
   }
- 
-  def renderQuery(queryOutput: Seq[Map[String, String]], templateFile: String): NodeSeq = {
+
+
+  def renderQuery(templateFilename: String)(queryOutput: Seq[Map[String, String]]): NodeSeq = {
     val transformer = new QueryTransformer(queryOutput)
-    transformer(XML.loadFile(templateFile))
+    transformer(XML.loadFile(templateFilename))
+  }
+
+
+  def sendEmail(queryResults: Map[String, Future[NodeSeq]], generalTemplateFile: Elem)(section: Section) {
+    info("Processing query " + section.name)
+
+    val addresses: Array[String] = section.options.get("addresses").getOrElse(throw new ConfigException("%s needs to include \"addresses\" attribute" format (section.name))).split(",").map(_.trim)
+    val queryNames: Array[String]  = section.options.get("query_names").getOrElse(throw new ConfigException("%s needs to include \"query_names\" attribute" format (section.name))).split(",").map(_.trim)
+    val templateFilename = section.options.get("template").getOrElse(throw new ConfigException("%s needs to include \"template\" attribute" format (section.name)))
+    
+    val queryOutputs: List[NodeSeq] = queryNames.toList map (queryResults(_).get())
+
+    val transformer = new EmailTransformer(queryOutputs)
+    val generalTransformer = new GeneralTransformer(transformer(XML.loadFile(templateFilename)))
+    val emailContent =  generalTransformer(generalTemplateFile)
+   
+    println(emailContent)
   }
 }
+
+class EmailTransformer(queryResults: List[NodeSeq]) extends Transformer {
+  $(".query-result") { node =>
+    queryResults.flatMap { r =>
+      new Transform(node) {
+        $(".query").contents = r
+      }
+    }
+  }
+}
+
 
 class QueryTransformer(queryResult: Seq[Map[String, String]]) extends Transformer {
   $(".result-row") { node =>
@@ -79,5 +111,19 @@ class QueryTransformer(queryResult: Seq[Map[String, String]]) extends Transforme
     }
   }
 }
-    
 
+
+class GeneralTransformer(email: NodeSeq) extends Transformer {
+  $(".content").contents = email
+}
+
+
+case class DBConnectionSettings(val host: String, val user: String, val password: String = "", val port: Int = 3306)
+
+object DBConnectionSettings {
+  def apply(section: Section): DBConnectionSettings =
+    new DBConnectionSettings(section.options.get("db.host").getOrElse(throw new ConfigException("%s needs to include \"db.host\" attribute" format (section.name))),
+                             section.options.get("db.username").getOrElse(throw new ConfigException("%s needs to include \"db.username\" attribute" format (section.name))),
+                             section.options.getOrElse("db.password", ""),
+                             section.options.getOrElse("db.port", "3306").toInt)
+}
